bcebec93188b411c6538e1f820df21d6c8033876
Will <willwatkins9@gmail.com>
2018-01-18 17:20:11 -0500

    Refactor code for moving pyramid left/right

--- js/bundle.js
+++ js/bundle.js
@@ -361,67 +361,33 @@ class Pyramid extends Tetrimino {
   }
 
   canMoveLeft() {
-    if (this.rotationPos === 0) {
-      return (this.x > 0 &&
-        this.well.getBlock([(this.x - 1), (this.y + 1)]).status === 'empty' &&
-        this.well.getBlock([this.x, this.y]).status === 'empty'
-      );
+    if (this.x === 0) {
+      return false;
     }
-
-    if (this.rotationPos === 1) {
-      return (this.x > 0 &&
-        this.well.getBlock([(this.x - 1), this.y]).status === 'empty' &&
-        this.well.getBlock([(this.x - 1), (this.y + 1)]).status === 'empty' &&
-        this.well.getBlock([(this.x - 1), (this.y + 2)]).status === 'empty'
-      );
-    }
-
-    if (this.rotationPos === 2) {
-      return (this.x > 0 &&
-        this.well.getBlock([(this.x - 1), this.y]).status === 'empty' &&
-        this.well.getBlock([(this.x), (this.y + 1)]).status === 'empty'
-      );
-    }
-
-    if (this.rotationPos === 3) {
-      return (this.x > 0 &&
-        this.well.getBlock([(this.x - 1), (this.y + 1)]).status === 'empty' &&
-        this.well.getBlock([this.x, this.y]).status === 'empty' &&
-        this.well.getBlock([this.x, (this.y + 2)]).status === 'empty'
-      );
-    }
+    let result = true;
+    this.blockCoords.forEach((coord) => {
+      let newX = coord[0] - 1;
+      let y = coord[1];
+      if (this.well.getBlock([newX, y]).status === 'filled') {
+        result = false;
+      }
+    });
+    return result;
   }
 
   canMoveRight() {
-    if (this.rotationPos === 0) {
-      return ((this.x + 3) < 10 &&
-      this.well.getBlock([(this.x + 3), (this.y + 1)]).status === 'empty' &&
-      this.well.getBlock([this.x + 2, this.y]).status === 'empty'
-      );
+    if ((this.rotationPos % 2 === 0 && (this.x + 3) === 10) || (this.rotationPos % 2 === 1) && (this.x + 2) === 10) {
+      return false;
     }
-
-    if (this.rotationPos === 1) {
-      return ((this.x + 2) < 10 &&
-      this.well.getBlock([(this.x + 2), (this.y + 1)]).status === 'empty' &&
-      this.well.getBlock([(this.x + 1), this.y]).status === 'empty' &&
-      this.well.getBlock([(this.x + 1), (this.y + 2)]).status === 'empty'
-      );
-    }
-
-    if (this.rotationPos === 2) {
-      return ((this.x + 3) < 10 &&
-        this.well.getBlock([(this.x + 3), this.y]).status === 'empty' &&
-        this.well.getBlock([this.x + 2, (this.y + 1)]).status === 'empty'
-      );
-    }
-
-    if (this.rotationPos === 3) {
-      return ((this.x + 2) < 10 &&
-        this.well.getBlock([(this.x + 2), this.y]).status === 'empty' &&
-        this.well.getBlock([(this.x + 2), (this.y + 1)]).status === 'empty' &&
-        this.well.getBlock([(this.x + 2), (this.y + 2)]).status === 'empty'
-      );
-    }
+    let result = true;
+    this.blockCoords.forEach((coord) => {
+      let newX = coord[0] + 1;
+      let y = coord[1];
+      if (this.well.getBlock([newX, y]).status === 'filled') {
+        result = false;
+      }
+    });
+    return result;
   }
 
   rotateClockwise() {
@@ -435,62 +401,36 @@ class Pyramid extends Tetrimino {
       xFactor = 1;
       yFactor = 0;
       clockwiseCoords = [[0, 0], [1, -1], [1, 1], [-1, 1]]
-      for (let i = 0; i < 4; i++) {
-        newBlockCoords[i][0] = this.blockCoords[i][0] + clockwiseCoords[i][0];
-        newBlockCoords[i][1] = this.blockCoords[i][1] + clockwiseCoords[i][1];
-      }
-      newBlockCoords.forEach((coord) => {
-        if (coord[0] < 0 || coord[0] > 9 || coord[1] < 0 || coord[1] > 19 || this.well.getBlock(coord).status === 'filled') {
-          canRotate = false
-        }
-      });
+
     }
 
     if (this.rotationPos === 1) {
       xFactor = -1;
       yFactor = 1;
       clockwiseCoords = [[0, 0], [1, 1], [-1, 1], [-1, -1]]
-      for (let i = 0; i < 4; i++) {
-        newBlockCoords[i][0] = this.blockCoords[i][0] + clockwiseCoords[i][0];
-        newBlockCoords[i][1] = this.blockCoords[i][1] + clockwiseCoords[i][1];
-      }
-      newBlockCoords.forEach((coord) => {
-        if (coord[0] < 0 || coord[0] > 9 || coord[1] < 0 || coord[1] > 19 || this.well.getBlock(coord).status === 'filled') {
-          canRotate = false
-        }
-      });
     }
 
     if (this.rotationPos === 2) {
       xFactor = 0;
       yFactor = -1;
       clockwiseCoords = [[0, 0], [-1, 1], [-1, -1], [1, -1]]
-      for (let i = 0; i < 4; i++) {
-        newBlockCoords[i][0] = this.blockCoords[i][0] + clockwiseCoords[i][0];
-        newBlockCoords[i][1] = this.blockCoords[i][1] + clockwiseCoords[i][1];
-      }
-      newBlockCoords.forEach((coord) => {
-        if (coord[0] < 0 || coord[0] > 9 || coord[1] < 0 || coord[1] > 19 || this.well.getBlock(coord).status === 'filled') {
-          canRotate = false
-        }
-      });
     }
 
     if (this.rotationPos === 3) {
       xFactor = 0;
       yFactor = 0;
       clockwiseCoords = [[0, 0], [-1, -1], [1, -1], [1, 1]]
-      for (let i = 0; i < 4; i++) {
-        newBlockCoords[i][0] = this.blockCoords[i][0] + clockwiseCoords[i][0];
-        newBlockCoords[i][1] = this.blockCoords[i][1] + clockwiseCoords[i][1];
-      }
-      newBlockCoords.forEach((coord) => {
-        if (coord[0] < 0 || coord[0] > 9 || coord[1] < 0 || coord[1] > 19 || this.well.getBlock(coord).status === 'filled') {
-          canRotate = false
-        }
-      });
     }
 
+    for (let i = 0; i < 4; i++) {
+      newBlockCoords[i][0] = this.blockCoords[i][0] + clockwiseCoords[i][0];
+      newBlockCoords[i][1] = this.blockCoords[i][1] + clockwiseCoords[i][1];
+    }
+    newBlockCoords.forEach((coord) => {
+      if (coord[0] < 0 || coord[0] > 9 || coord[1] < 0 || coord[1] > 19 || this.well.getBlock(coord).status === 'filled') {
+        canRotate = false
+      }
+    });
     if (canRotate) {
       this.clear();
       this.blockCoords = newBlockCoords;
