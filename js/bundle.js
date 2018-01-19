bc980635ef77674abc70ae0e9d2c948f4835ca32
Will <willwatkins9@gmail.com>
2018-01-19 14:19:10 -0500

    Fix errors when rotating square

--- js/bundle.js
+++ js/bundle.js
@@ -433,17 +433,6 @@ class Square extends Tetrimino {
     this.blockCoords = [[0, 0], [1, 0], [0, 1], [1, 1]];
   }
 
-  rerender(arr) {
-    this.ctx.clearRect((this.x * 40), (this.y * 40), 80, 80);
-    this.x += arr[0];
-    this.y += arr[1];
-    this.blockCoords.forEach((coord) => {
-      coord[0] += arr[0];
-      coord[1] += arr[1];
-    });
-    this.ctx.fillRect((this.x * 40), (this.y * 40), 80, 80);
-  }
-
   canMoveDown() {
     return (this.y < 18 && this.getBlocksBelow().length === 0)
   }
@@ -460,6 +449,10 @@ class Square extends Tetrimino {
       this.well.getBlock([(this.x + 2), this.y]).status === 'empty' &&
       this.well.getBlock([(this.x + 2), (this.y + 1)]).status === 'empty'
     );
+  }
+
+  handleRotation(dir) {
+    return
   }
 
 }
@@ -1236,8 +1229,7 @@ class Game {
   }
 
   setupNewPiece() {
-    // this.currentTetrimino = new allPieces[this.getRandomInt(7)](this.ctx, this.well);
-    this.currentTetrimino = new RightSnake(this.ctx, this.well);
+    this.currentTetrimino = new allPieces[this.getRandomInt(7)](this.ctx, this.well);
     this.falling = window.setInterval(this.handleVerticalMovement, 500);
   }
 
--- js/tetriminoes/square.js
+++ js/tetriminoes/square.js
@@ -8,17 +8,6 @@ class Square extends Tetrimino {
     this.blockCoords = [[0, 0], [1, 0], [0, 1], [1, 1]];
   }
 
-  rerender(arr) {
-    this.ctx.clearRect((this.x * 40), (this.y * 40), 80, 80);
-    this.x += arr[0];
-    this.y += arr[1];
-    this.blockCoords.forEach((coord) => {
-      coord[0] += arr[0];
-      coord[1] += arr[1];
-    });
-    this.ctx.fillRect((this.x * 40), (this.y * 40), 80, 80);
-  }
-
   canMoveDown() {
     return (this.y < 18 && this.getBlocksBelow().length === 0)
   }
@@ -35,6 +24,10 @@ class Square extends Tetrimino {
       this.well.getBlock([(this.x + 2), this.y]).status === 'empty' &&
       this.well.getBlock([(this.x + 2), (this.y + 1)]).status === 'empty'
     );
+  }
+
+  handleRotation(dir) {
+    return
   }
 
 }
