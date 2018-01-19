de1f85b351a657886e9ef3f992ab8a4598964071
Will <willwatkins9@gmail.com>
2018-01-19 12:51:29 -0500

    Add rotation to gamma piece

--- js/tetriminoes/gamma.js
+++ js/tetriminoes/gamma.js
@@ -5,37 +5,154 @@ class Gamma extends Tetrimino {
   constructor(ctx, well) {
     super(ctx, well);
     ctx.fillStyle = 'blue';
-    this.blockCoords = [[0, 0], [0, 1], [1, 1], [2, 1]]
+    this.blockCoords = [[0, 0], [0, 1], [1, 1], [2, 1]];
   }
 
   rerender(arr) {
-    this.ctx.clearRect((this.x * 40), (this.y * 40), 40, 40);
-    this.ctx.clearRect(((this.x) * 40), ((this.y + 1) * 40), 120, 40);
     this.x += arr[0];
     this.y += arr[1];
     this.blockCoords.forEach((coord) => {
       coord[0] += arr[0];
       coord[1] += arr[1];
+      this.ctx.fillRect((coord[0] * 40), coord[1] * 40, 40, 40);
     });
-    this.ctx.fillRect((this.x * 40), (this.y * 40), 40, 40);
-    this.ctx.fillRect((this.x * 40), ((this.y + 1) * 40), 120, 40);
   }
 
   canMoveDown() {
-    return (this.y < 18 && this.getBlocksBelow().length === 0)
+    if (this.rotationPos % 2 === 0) {
+      return (this.y < 18 && this.getBlocksBelow().length === 0);
+    }
+    if (this.rotationPos % 2 === 1) {
+      return (this.y < 17 && this.getBlocksBelow().length === 0);
+    }
   }
 
   canMoveLeft() {
-    return (this.x > 0 &&
-      this.well.getBlock([(this.x - 1), this.y]).status === 'empty' &&
-      this.well.getBlock([(this.x - 1), (this.y + 1)]).status === 'empty'
-    );
+    if (this.rotationPos === 0) {
+      return (this.x > 0 &&
+        this.well.getBlock([(this.x - 1), this.y]).status === 'empty' &&
+        this.well.getBlock([(this.x - 1), (this.y + 1)]).status === 'empty'
+      );
+    }
+    if (this.rotationPos === 1) {
+      return (this.x > 0 &&
+        this.well.getBlock([(this.x - 1), this.y]).status === 'empty' &&
+        this.well.getBlock([(this.x - 1), (this.y + 1)]).status === 'empty' &&
+        this.well.getBlock([(this.x - 1), (this.y + 2)]).status === 'empty'
+      );
+    }
+    if (this.rotationPos === 2) {
+      return (this.x > 0 &&
+        this.well.getBlock([(this.x - 1), this.y]).status === 'empty' &&
+        this.well.getBlock([(this.x + 1), (this.y + 1)]).status === 'empty'
+      );
+    }
+    if (this.rotationPos === 3) {
+      return (this.x > 0 &&
+        this.well.getBlock([this.x, this.y]).status === 'empty' &&
+        this.well.getBlock([this.x, (this.y + 1)]).status === 'empty' &&
+        this.well.getBlock([(this.x - 1), (this.y + 2)]).status === 'empty'
+      );
+    }
   }
 
   canMoveRight() {
-    return ((this.x + 3) < 10 &&
-      this.well.getBlock([(this.x + 3), (this.y + 1)]).status === 'empty'
-    );
+    if (this.rotationPos === 0) {
+      return ((this.x + 3) < 10 &&
+        this.well.getBlock([(this.x + 1), this.y]).status === 'empty' &&
+        this.well.getBlock([(this.x + 3), (this.y + 1)]).status === 'empty'
+      );
+    }
+    if (this.rotationPos === 1) {
+      return ((this.x + 2) < 10 &&
+        this.well.getBlock([(this.x + 2), this.y]).status === 'empty' &&
+        this.well.getBlock([(this.x + 1), (this.y + 1)]).status === 'empty' &&
+        this.well.getBlock([(this.x + 1), (this.y + 2)]).status === 'empty'
+      );
+    }
+    if (this.rotationPos === 2) {
+      return ((this.x + 3) < 10 &&
+        this.well.getBlock([(this.x + 3), this.y]).status === 'empty' &&
+        this.well.getBlock([(this.x + 3), (this.y + 1)]).status === 'empty'
+      );
+    }
+    if (this.rotationPos === 3) {
+      return ((this.x + 2) < 10 &&
+        this.well.getBlock([(this.x + 2), this.y]).status === 'empty' &&
+        this.well.getBlock([(this.x + 2), (this.y + 1)]).status === 'empty' &&
+        this.well.getBlock([(this.x + 2), (this.y + 2)]).status === 'empty'
+      );
+    }
+  }
+
+  rotateClockwise() {
+    let rotationCoords;
+    let canRotate = true;
+    let xFactor;
+    let yFactor;
+
+    if (this.rotationPos === 0) {
+      xFactor = 1;
+      yFactor = 0;
+      rotationCoords = [[2, 0], [1, -1], [0, 0], [-1, 1]]
+    }
+
+    if (this.rotationPos === 1) {
+      xFactor = -1;
+      yFactor = 1;
+      rotationCoords = [[0, 2], [1, 1], [0, 0], [-1, -1]];
+    }
+
+    if (this.rotationPos === 2) {
+      xFactor = 0;
+      yFactor = -1;
+      rotationCoords = [[-2, 0], [-1, 1], [0, 0], [1, -1]]
+    }
+
+    if (this.rotationPos === 3) {
+      xFactor = 0;
+      yFactor = 0;
+      rotationCoords = [[0, -2], [-1, -1], [0, 0], [1, 1]];
+    }
+
+    return {xFactor, yFactor, rotationCoords}
+
+  }
+
+
+  rotateCounterClockwise() {
+    let rotationCoords;
+    let canRotate = true;
+    let xFactor;
+    let yFactor;
+
+    if (this.rotationPos === 0) {
+      xFactor = 0;
+      yFactor = 0;
+      rotationCoords = [[0, 2], [1, 1], [0, 0], [-1, -1]]
+    }
+
+    if (this.rotationPos === 1) {
+      xFactor = -1;
+      yFactor = 0;
+      rotationCoords = [[-2, 0], [-1, 1], [0, 0], [1, -1]];
+    }
+
+    if (this.rotationPos === 2) {
+      xFactor = 1;
+      yFactor = -1;
+      rotationCoords = [[0, -2], [-1, -1], [0, 0], [1, 1]]
+    }
+
+    if (this.rotationPos === 3) {
+      xFactor = 0;
+      yFactor = 1;
+      rotationCoords = [[2, 0], [1, -1], [0, 0], [-1, 1]];
+    }
+
+    return {xFactor, yFactor, rotationCoords}
+
+
   }
 
 }
